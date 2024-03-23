86650da9977a4a5302b22f0f02666a9dd4074a2b
johnvictorfs <37747572+johnvictorfs@users.noreply.github.com>
2024-03-23 18:11:23 -0300

    fix loading bar color

--- components/layout/LoadingBar.vue
+++ components/layout/LoadingBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-400">
-    <div v-bind:class="`bg-${props.color || 'blue'}-600 h-2.5 rounded-full transition-all duration-500 ease-in-out`" v-bind:style="`width: ${percentage}%`"></div>
+    <div v-bind:class="`${backgroundClassName} h-2.5 rounded-full transition-all duration-500 ease-in-out`" v-bind:style="`width: ${percentage}%`"></div>
   </div>
 </template>
 
@@ -15,4 +15,31 @@ const props = defineProps<{
 }>()
 
 const percentage = computed(() => (props.value / props.max) * 100)
+
+const backgroundClassName = computed(() => {
+  switch (props.color) {
+    case 'red':
+      return 'bg-red-600'
+    case 'yellow':
+      return 'bg-yellow-600'
+    case 'green':
+      return 'bg-green-600'
+    case 'blue':
+      return 'bg-blue-600'
+    case 'indigo':
+      return 'bg-indigo-600'
+    case 'purple':
+      return 'bg-purple-600'
+    case 'pink':
+      return 'bg-pink-600'
+    case 'gray':
+      return 'bg-gray-600'
+    case 'black':
+      return 'bg-black'
+    case 'white':
+      return 'bg-white'
+    default:
+      return 'bg-blue-600'
+  }
+})
 </script>
